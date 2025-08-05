9c49f806fb599e4e520b8c402cd0854e3bfd9c0d
Gaurav Kumar <gauravkr0211@gmail.com>
2025-08-05 09:55:07 +0530

    Linear Search

--- src/main/java/org/dsa/Searching/LinearSearch.java
+++ src/main/java/org/dsa/Searching/LinearSearch.java
@@ -1,4 +1,40 @@
 package org.dsa.Searching;
+import java.util.Scanner;
 
 public class LinearSearch {
+
+    static void display(int[] arr){
+        for(int x : arr){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+    }
+
+    static void search(int [] arr, int k){
+        boolean flag = false;
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i] == k){
+                System.out.println("Element found at index : "+i);
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            System.out.println("Element not found");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array Size");
+        int x = sc.nextInt();
+        System.out.println("Enter Array Element");
+        int[] arr = new int[x];
+        for(int i =0; i< x; i++){
+            arr[i] = sc.nextInt();
+        }
+        search(arr, 6);
+    }
 }
