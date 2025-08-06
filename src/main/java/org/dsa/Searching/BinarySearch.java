5f868808ed99f3414664f2f0bb4ca261f2aa25ed
Gaurav Kumar <gauravkr0211@gmail.com>
2025-08-06 10:53:15 +0530

    Binary Search Impl

---
+++ src/main/java/org/dsa/Searching/BinarySearch.java
@@ -0,0 +1,28 @@
+package org.dsa.Searching;
+
+public class BinarySearch {
+    public static int binarySearch(int[] arr,int target){
+        int low =0;
+        int high = arr.length - 1;
+        while(low <= high){
+            int mid  = (low + high) /2;
+            if(arr[mid] == target){
+                return mid;
+            }else if(target > arr[mid]){
+                low = mid +1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {12, 43, 78, 96, 106, 190, 234, 432};
+        int ans = binarySearch(arr, 432);
+        if(ans >=0){
+            System.out.println("Element found at :"+ ans);
+        }else{
+            System.out.println("Element not found");
+        }
+    }
+}
