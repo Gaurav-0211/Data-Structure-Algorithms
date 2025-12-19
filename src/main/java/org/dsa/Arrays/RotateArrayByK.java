6d5fa7638220e0aed3e913c73c38f954694e4c76
Gaurav Kumar <gauravkr0211@gmail.com>
2025-12-19 12:46:30 +0530

    _Sol: Move Zeroes to the end of Array

--- src/main/java/org/dsa/Arrays/RotateArrayByK.java
+++ src/main/java/org/dsa/Arrays/RotateArrayByK.java
@@ -17,8 +17,27 @@ public class RotateArrayByK {
         }
 
     }
+    // Move all zero to the end of array
+    public static void moveToEnd(int[] arr){
+        int[] ans = new int[arr.length];
+        int k = 0;
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i]==1){
+                ans[k++] = 1;
+            }
+        }
+        for(int i = k; i< arr.length; i++){
+            ans[k++] = 0;
+        }
+        for(int x : ans){
+            System.out.print(x+" ");
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {12, 34, 53, 17, 81, 55, 67};
+        int[] arr1 = {1, 0, 0, 1, 1, 0, 1, 0};
         rotateByK(arr, 3);
+        moveToEnd(arr1);
     }
 }
