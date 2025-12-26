92a9b5c1d076255f550236a16bdb7855613ad8e2
Gaurav Kumar <gauravkr0211@gmail.com>
2025-12-26 17:18:53 +0530

    _Sol: Binary Search

--- src/main/java/org/dsa/Arrays/TargetSum.java
+++ src/main/java/org/dsa/Arrays/TargetSum.java
@@ -19,6 +19,30 @@ public class TargetSum {
         return new int[]{};
     }
 
+    // Binary Search
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] arr = {7, 22, 34, 71, 80, 95, 100};
         int[] ans = findTargetIndex(arr, 122);
