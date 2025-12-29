cd02b584e8e1b4ded07976c8178ef20c9929afde
Gaurav Kumar <gauravkr0211@gmail.com>
2025-12-29 18:06:47 +0530

    _Fix: Reverse

--- src/main/java/org/dsa/LinkedList/SinglyList.java
+++ src/main/java/org/dsa/LinkedList/SinglyList.java
@@ -159,6 +159,21 @@ class SinglyLinkedList{
         return num;
     }
 
+    // Reverse a linked list
+    void reverse(){
+        Node curr = head;
+        Node prev = null;
+        Node next = null;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
 }
 
 public class SinglyList {
