fd4488eca966080ad79dcf966fd0032fc81eaf1c
Gaurav Kumar <gauravkr0211@gmail.com>
2025-12-30 17:54:14 +0530

    _Fix: Negative Grid

--- src/main/java/org/dsa/Arrays/Solution.java
+++ src/main/java/org/dsa/Arrays/Solution.java
@@ -3,22 +3,39 @@ package org.dsa.Arrays;
 import java.util.Arrays;
 
 public class Solution {
-        // 3074 Apple redistribution into boxes
-        public static int minimumBoxes(int[] apple, int[] capacity) {
-            int sum = 0;
-            int total = 0;
-            int ans = 0;
-            for (int x : apple) {
-                sum += x;
-            }
-            Arrays.sort(capacity);
-
-            for (int i = capacity.length - 1; i >= 0 && total < sum; i--) {
-                ans++;
-                total += capacity[i];
-            }
-            return ans;
+    // 3074 Apple redistribution into boxes
+    public static int minimumBoxes(int[] apple, int[] capacity) {
+        int sum = 0;
+        int total = 0;
+        int ans = 0;
+        for (int x : apple) {
+            sum += x;
         }
+        Arrays.sort(capacity);
+
+        for (int i = capacity.length - 1; i >= 0 && total < sum; i--) {
+            ans++;
+            total += capacity[i];
+        }
+        return ans;
+    }
+
+    // Count Negative grid in a sorted matrix
+    public static int countNegatives(int[][] grid) {
+        int ans = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (grid[i][j] < 0) {
+                ans += (m - i);
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         int[] apple = {2, 5, 3, 6};
